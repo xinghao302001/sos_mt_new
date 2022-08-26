b7f0be823cb3a5f9b12ef14eea8ef13e92e3336b
Xinghao93 <wxhlovelife@gmail.com>
2022-08-27 03:17:50 +0800

    update svo_openIE

--- svo_openIE.py
+++ svo_openIE.py
@@ -7,9 +7,23 @@ properties = {
     "openie.max_entailments_per_clause":150,
     "openie.threads":10
 }
-client = StanfordOpenIE(install_dir_path='4.4.0')
+client = StanfordOpenIE(install_dir_path='4.1.0')
 
 
 def getSVOs(sentence):
-    triple_res = client.annotate(sentence,properties=properties)
-    return triple_res
+    triple_res_list = []
+    triples = client.annotate(sentence, properties=properties)
+    for triple in triples:
+        sub_ = triple['subject']
+        obj_ = triple['object']
+        rel_ = triple['relation']
+        triple_res_list.append((sub_,rel_,obj_))
+    return triple_res_list
+
+
+# if __name__ == "__main__":
+#     text = "Tom likes dogs, and dogs are important for his life."
+#     res = getSVOs(text)
+#     for triple in res:
+#         print(triple)
+#     # print(res)
